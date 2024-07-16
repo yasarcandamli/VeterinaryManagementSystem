75fc4d6c1ebfc88bc62e35d7291f41335b28ba67
yasarcandamli <yasarcandamli@gmail.com>
2024-07-16 10:55:33 +0300

    Vaccine entity created

--- src/main/java/dev/patika/VeterinaryManagementSystem/entity/Vaccine.java
+++ src/main/java/dev/patika/VeterinaryManagementSystem/entity/Vaccine.java
@@ -6,33 +6,32 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Entity
-@Table(name = "customers")
+@Table(name = "vaccines")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Customer {
+public class Vaccine {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "customer_id")
+    @Column(name = "vaccine_id")
     private long id;
 
     @NotNull
-    @Column(name = "customer_name")
+    @Column(name = "vaccine_name")
     private String name;
 
     @NotNull
-    @Column(name = "customer_phone")
-    private String phone;
+    @Column(name = "vaccine_code")
+    private String code;
 
     @NotNull
-    @Column(name = "customer_mail")
-    private String mail;
+    @Column(name = "vaccine_protection_start_date")
+    private LocalDate protectionStartDate;
 
     @NotNull
-    @Column(name = "customer_address")
-    private String address;
-
-    @Column(name = "customer_city")
-    private String city;
+    @Column(name = "vaccine_protection_end_date")
+    private LocalDate protectionEndDate;
 }
