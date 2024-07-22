d212264d21b964d9d2518af8fd10aa03ade6e2fc
yasarcandamli <yasarcandamli@gmail.com>
2024-07-22 10:25:50 +0300

    GetMappings, PutMapping and DeleteMapping added to the DoctorController

--- src/main/java/dev/patika/VeterinaryManagementSystem/api/DoctorController.java
+++ src/main/java/dev/patika/VeterinaryManagementSystem/api/DoctorController.java
@@ -2,12 +2,19 @@ package dev.patika.VeterinaryManagementSystem.api;
 
 import dev.patika.VeterinaryManagementSystem.business.abstracts.IDoctorService;
 import dev.patika.VeterinaryManagementSystem.core.config.modelMapper.IModelMapperService;
+import dev.patika.VeterinaryManagementSystem.core.result.Result;
 import dev.patika.VeterinaryManagementSystem.core.result.ResultData;
 import dev.patika.VeterinaryManagementSystem.core.utility.ResultHelper;
+import dev.patika.VeterinaryManagementSystem.dto.CursorResponse;
 import dev.patika.VeterinaryManagementSystem.dto.request.doctor.DoctorSaveRequest;
+import dev.patika.VeterinaryManagementSystem.dto.request.doctor.DoctorUpdateRequest;
+import dev.patika.VeterinaryManagementSystem.dto.request.vaccine.VaccineUpdateRequest;
 import dev.patika.VeterinaryManagementSystem.dto.response.doctor.DoctorResponse;
+import dev.patika.VeterinaryManagementSystem.dto.response.vaccine.VaccineResponse;
 import dev.patika.VeterinaryManagementSystem.entity.Doctor;
+import dev.patika.VeterinaryManagementSystem.entity.Vaccine;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,5 +35,49 @@ public class DoctorController {
         Doctor saveDoctor = this.modelMapper.forRequest().map(doctorSaveRequest, Doctor.class);
         this.doctorService.save(saveDoctor);
         return ResultHelper.created(this.modelMapper.forResponse().map(saveDoctor, DoctorResponse.class));
+    }
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<CursorResponse<DoctorResponse>> cursor(
+            @RequestParam(name = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(name = "pageSize", required = false, defaultValue = "10") int pageSize
+    ) {
+        Page<DoctorResponse> doctorResponsePage = doctorService.cursor(page, pageSize)
+                .map(doctor -> {
+                    DoctorResponse response = modelMapper.forResponse().map(doctor, DoctorResponse.class);
+                    return response;
+                });
+
+        return ResultHelper.cursor(doctorResponsePage);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<DoctorResponse> get(@PathVariable("id") Long id) {
+        Doctor doctor = this.doctorService.get(id);
+        DoctorResponse doctorResponse = this.modelMapper.forResponse().map(doctor, DoctorResponse.class);
+        return ResultHelper.success(doctorResponse);
+    }
+
+    @PutMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public ResultData<DoctorResponse> update(@Valid @RequestBody DoctorUpdateRequest doctorUpdateRequest) {
+        Doctor existingDoctor = this.doctorService.get(doctorUpdateRequest.getId());
+
+        this.modelMapper.forRequest().getConfiguration().setSkipNullEnabled(true);
+        this.modelMapper.forRequest().map(doctorUpdateRequest, existingDoctor);
+
+        Doctor updatedDoctor = this.doctorService.update(existingDoctor);
+
+        DoctorResponse doctorResponse = this.modelMapper.forResponse().map(updatedDoctor, DoctorResponse.class);
+        return ResultHelper.success(doctorResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Result delete(@PathVariable("id") Long id) {
+        this.doctorService.delete(id);
+        return ResultHelper.ok();
     }
 }
